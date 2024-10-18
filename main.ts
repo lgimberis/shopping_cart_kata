a1f9c84909b692cc5fb1c9fada64e2d4bd7accc9
Lukas Gimberis <lukasgimberis@gmail.com>
2024-10-18 12:14:03 +0100

    Implement addition of a single item to the shopping cart

--- main.ts
+++ main.ts
@@ -1,9 +1,54 @@
 import { Product } from "./product.ts";
+import type { ProductService } from "./productService.ts";
+
+export type ShoppingCartRender = {
+  products: {
+    name: string;
+    priceWithVAT: number;
+    quantity: number;
+  }[];
+  numberOfProducts: number;
+  totalPrice: number;
+};
 
 export class ShoppingCart {
-  constructor() {}
+  productService: ProductService;
+  products: Map<string, { product: Product; quantity: number }>;
+  constructor(productService: ProductService) {
+    this.productService = productService;
+    this.products = new Map();
+  }
 
-  render(): Map<string, Product> {
-    return new Map();
+  render(): ShoppingCartRender {
+    const products = [];
+    let numberOfProducts = 0;
+    let totalPrice = 0.0;
+    for (const [name, { product, quantity }] of this.products.entries()) {
+      const priceWithVAT = product.getFinalPrice();
+      products.push({ name, priceWithVAT, quantity });
+      numberOfProducts += quantity;
+      totalPrice += priceWithVAT * quantity;
+    }
+    return {
+      products,
+      numberOfProducts,
+      totalPrice,
+    };
+  }
+
+  add(name: string) {
+    if (this.products.has(name)) {
+      // Existing product
+      const product = this.products.get(name);
+      product!.quantity += 1;
+      this.products.set(name, product!);
+      return;
+    }
+    // New product
+    const product = this.productService.get(name);
+    if (!product) {
+      throw new Error(`Product ${name} does not exist in service`);
+    }
+    this.products.set(name, { product, quantity: 1 });
   }
 }
--- main_test.ts
+++ main_test.ts
@@ -1,7 +1,27 @@
 import { assertEquals } from "@std/assert";
-import { ShoppingCart } from "./main.ts";
+import { ShoppingCart, ShoppingCartRender } from "./main.ts";
+import { ProductService } from "./productService.ts";
+import { TestProductSource } from "./testProductSource.ts";
 
 Deno.test("Test that empty cart is empty", function emptyTest() {
-  const cart = new ShoppingCart();
-  assertEquals(cart.render(), new Map());
+  const productService = new ProductService(new TestProductSource());
+  const cart = new ShoppingCart(productService);
+  const expected: ShoppingCartRender = {
+    products: [],
+    totalPrice: 0,
+    numberOfProducts: 0,
+  };
+  assertEquals(cart.render(), expected);
+});
+
+Deno.test("Add a single item to the shopping cart", function addOneItem() {
+  const productService = new ProductService(new TestProductSource());
+  const cart = new ShoppingCart(productService);
+  cart.add("Corn");
+  const expected = {
+    products: [{ name: "Corn", priceWithVAT: 1.5, quantity: 1 }],
+    numberOfProducts: 1,
+    totalPrice: 1.5,
+  };
+  assertEquals(cart.render(), expected);
 });
