24652da590694bae9f5ada95d2af866f05f72d61
Lukas Gimberis <lukasgimberis@gmail.com>
2024-10-18 09:27:22 +0100

    Add tax test to product tests

--- shopping_cart/product_test.ts
+++ shopping_cart/product_test.ts
@@ -5,3 +5,8 @@ Deno.test("Margins correctly applied", function marginTest() {
   const product = new Product("Iceberg", 1.55, 0.15, TaxCategories.NORMAL);
   assertEquals(product.getPreTaxPrice(), 1.79);
 });
+
+Deno.test("Tax correctly applied", function taxTest() {
+  const product = new Product("Iceberg", 1.55, 0.15, TaxCategories.NORMAL);
+  assertEquals(product.getFinalPrice(), 2.17);
+});
